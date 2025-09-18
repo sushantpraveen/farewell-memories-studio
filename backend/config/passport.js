10b13bcd4580cd1401f24018d08a8ff0863ee651
Pentacloud-Consulting <shabahatsyed101@gmail.com>
2025-09-18 12:07:50 +0530

    pasportjs sample

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -11,7 +11,7 @@ export const configurePassport = () => {
         {
           clientID: process.env.GOOGLE_CLIENT_ID,
           clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-          callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:4000/api/auth/google/callback',
+          callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://signaturedaytshirt.com/api/auth/google/callback',
           proxy: true,
         },
       async (accessToken, refreshToken, profile, done) => {
