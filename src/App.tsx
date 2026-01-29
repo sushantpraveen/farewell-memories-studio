36c41344b81d3664429d767fc6e1762cc31b7f24
Sushant Praveen <sushantprem2820@gmail.com>
2026-01-29 17:50:42 +0530

    implemnted GA4

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { CollageProvider } from "./context/CollageContext";
 import { ProtectedRoute } from "./components/ProtectedRoute";
+import RouteTracker from "./RouteTracker";
 import StructuredData from "./components/seo/StructuredData";
 import PerformanceOptimizer from "./components/seo/PerformanceOptimizer";
 import Index from "./pages/Index";
@@ -47,6 +48,7 @@ const App = () => (
           <Toaster />
           <Sonner />
           <BrowserRouter>
+            <RouteTracker />
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/how-it-works" element={<HowItWorks />} />
